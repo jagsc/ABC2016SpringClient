6e20de941888891a82f18afafe0e124814bff01b
yuuki014 <b.d.cloud2385@gmail.com>
2015-12-13 00:41:33 +0900

    LayoutとActivity作成テスト(ソースコードほぼなし)

---
+++ mobile/src/main/java/jagsc/org/abc2016springclient/DemoActivity.java
@@ -0,0 +1,15 @@
+package jagsc.org.abc2016springclient;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by yuuki on 2015/12/12.
+ */
+public class DemoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+    }
+}
