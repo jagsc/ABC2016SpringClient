e2568433bac7cf3105a900c4e91a2f7845c312f8
yuuki014 <b.d.cloud2385@gmail.com>
2015-12-13 11:50:51 +0900

    LayoutとActivity作成テスト2(ソースコードほぼなし)

--- wear/src/main/java/jagsc/org/abc2016springclient/WearDemoActivity.java
+++ wear/src/main/java/jagsc/org/abc2016springclient/WearDemoActivity.java
@@ -3,13 +3,11 @@ package jagsc.org.abc2016springclient;
 import android.os.Bundle;
 import android.support.wearable.activity.WearableActivity;
 
-/**
- * Created by yuuki on 2015/12/12.
- */
+
 public class WearDemoActivity extends WearableActivity {
-    @Override
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_demo_wear);
-    }
+    }*/
 }
